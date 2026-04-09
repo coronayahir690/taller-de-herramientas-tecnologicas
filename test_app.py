5358e6f4bfceb396d79e9419c0d47e3d4bdfded5
Yahir Corona <coronayahir690@gmail.com>
2026-04-09 13:35:05 -0700

    Agregando tests

---
+++ test_app.py
@@ -0,0 +1,14 @@
+def test_suma():
+    assert 2 + 2 == 4
+
+
+def test_resta():
+    assert 5 - 3 == 2
+
+
+def test_multiplicacion():
+    assert 3 * 3 == 9
+
+
+def test_division():
+    assert 10 / 2 == 5
